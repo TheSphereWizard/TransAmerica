f37b548d752135cc69da84a4967fed24d135dd49
jcole8806 <jcole8806@CHHB2ZQDXV1.inst.hcpss.org>
2018-02-26 13:58:10 -0500

    These are the skeleton classes for the AI subgroup

---
+++ TransAmerica/src/ComputerPlayerEasy.java
@@ -0,0 +1,20 @@
+
+public class ComputerPlayerEasy extends Player {
+
+	Rail[] runTurn() {
+		return null;
+	}
+
+	void clearForNewRound() {
+		
+	}
+
+	void updatePlayer() {
+		
+	}
+
+	String playerName() {
+		return null;
+	}
+
+}
---
+++ TransAmerica/src/HumanPlayer.java
@@ -0,0 +1,26 @@
+/*
+ * HumanPlayer extends Player{
+Rail[] runTurn() {}
+String playerName() {}
+}
+
+ */
+public class HumanPlayer extends Player {
+
+	Rail[] runTurn() {
+		return null;
+	}
+
+	void clearForNewRound() {
+		
+	}
+
+	void updatePlayer() {
+		
+	}
+
+	String playerName() {
+		return null;
+	}
+
+}
