c1c0e9ca92ee9ffcc2dd5f6c9be5330fa7587bda
jcole8806 <jcole8806@CHHB2ZQDXV1.inst.hcpss.org>
2018-03-01 13:49:03 -0500

    Updated ComputerPlayer

--- TransAmerica/AI & Players/ComputerPlayer.java
+++ TransAmerica/AI & Players/ComputerPlayer.java
@@ -1,5 +1,12 @@
 
 public class ComputerPlayer extends Player {
+	
+	//Computer player should get copy of game state (but should not have access to other player’s hands)
+	//Pass in an array of scores, a grid, and a hand to computer players
+	
+	public ComputerPlayer() {
+		
+	}
 
 	Rail[] runTurn() {
 		return null;
