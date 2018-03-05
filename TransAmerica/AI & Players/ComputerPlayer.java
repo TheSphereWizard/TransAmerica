28455c9b3977ea645af508d4fb0df9411cad4735
jcole8806 <jcole8806@CHHB2ZQDXV1.inst.hcpss.org>
2018-03-05 14:09:36 -0500

    updated constructor

--- TransAmerica/AI & Players/ComputerPlayer.java
+++ TransAmerica/AI & Players/ComputerPlayer.java
@@ -4,9 +4,16 @@ public class ComputerPlayer extends Player {
 	
 	//Computer player should get copy of game state (but should not have access to other player’s hands)
 	//Pass in an array of scores, a grid, and a hand to computer players
+	private int[] scores;
+	private Grid grid;
+	private City[] cities;
+	
 	
 	public ComputerPlayer(int[] scores, Grid grid, City[] cities) {
 		super(null);
+		this.scores = scores;
+		this.grid = grid;
+		this.cities = cities;
 	}
 
 	Rail[] runTurn() {
