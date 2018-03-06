c55a111936ac4cb45c3e282afef47610f2c2d523
jrowle6776 <jrowle6776@CHHB2ZMKXV1.inst.hcpss.org>
2018-03-06 13:41:42 -0500

    sadffd

--- TransAmerica/UI/ComputerStrategyScreen.java
+++ TransAmerica/UI/ComputerStrategyScreen.java
@@ -6,25 +6,28 @@ import javax.swing.*;
 
 public class ComputerStrategyScreen extends JPanel{
 	
-	Image img;
-	ComputerStrategyScreen(){
-		JFrame frame = new JFrame();
-
-		frame.setContentPane(this);
-		frame.setSize(500, 500);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		
-		 img = Toolkit.getDefaultToolkit().createImage("Screen.PNG");
-		 
-		frame.setVisible(true);
-	}
+	JTextArea a = new JTextArea();
+	JTextArea b = new JTextArea();
+	JTextArea c = new JTextArea();
+	JTextArea d = new JTextArea();
+	JTextArea e = new JTextArea();
+	
 	public ComputerStrategyScreen(int gamesPlayed, int[] gamesWon, int[] gamesLost, int[] rank,
 			double[] winPercentage) {
+		this.add(a);
+		this.add(b);
+		this.add(c);
+		this.add(d);
+		this.add(e);
+		a.setText("Games Played \n" +gamesPlayed);
+		b.setText("Games Won \n" +gamesWon);
+		c.setText("Games Lost \n" +gamesLost);
+		d.setText("Rank \n" +rank);
+		e.setText("winPercentyage \n" +winPercentage);
 		
 	}
 	public static void main(String[] args){
-		new ComputerStrategyScreen();
+		
 	}
 
 }
