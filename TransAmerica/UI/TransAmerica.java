285929557f14a3a352495927df93a7d7d94646ba
jbosso8565 <jbosso8565@CHHB2ZKDXV1.inst.hcpss.org>
2018-03-06 13:41:03 -0500

    ew

--- TransAmerica/UI/TransAmerica.java
+++ TransAmerica/UI/TransAmerica.java
@@ -24,6 +24,7 @@ public class TransAmerica extends JFrame implements ActionListener{
 		this.setTitle("TransAmerica");
 		
 		errorMessage.setText("Error! Not Enough Players");
+		errorMessage.set
 		errorPanel.setLayout(new GridLayout(2,1));
 		errorPanel.add(errorMessage);
 		errorPanel.add(exit);
