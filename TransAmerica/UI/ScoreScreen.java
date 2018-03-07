c8128bb200879be95c1eae640d4183e5f79c558b
jbosso8565 <jbosso8565@CHHB2ZKDXV1.inst.hcpss.org>
2018-03-07 13:42:46 -0500

    asdf

--- TransAmerica/UI/ScoreScreen.java
+++ TransAmerica/UI/ScoreScreen.java
@@ -1,4 +1,23 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class ScoreScreen extends JFrame implements Ac {
+public class ScoreScreen extends JFrame implements ActionListener {
 
+	private JPanel contentPane = new JPanel();
+	
+	
+	ScoreScreen(Color playerColor , int playerScore) {
+		
+	}
+	
+	
+	
+	
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
 }
