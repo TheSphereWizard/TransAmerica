8cabb655152a4715bde0fa6cf0409320b4f52f33
kxu9969 <kxu9969@CHHB2ZPFXV1.inst.hcpss.org>
2018-03-07 13:58:30 -0500

    updated constructors

--- TransAmerica/AI & Players/HardStrategy.java
+++ TransAmerica/AI & Players/HardStrategy.java
@@ -2,9 +2,10 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 public class HardStrategy extends ComputerPlayer {
-
-	public HardStrategy(Color c, ArrayList<City> cities, int score, int[] scores, String name, Grid grid) {
-		super(c, cities, score, scores, name, grid);
+	int[] allScores;
+	public HardStrategy(Color c, ArrayList<City> cities, int score, int[] allScores, String name, Grid grid) {
+		super(c, cities, score, name, grid);
+		this.allScores = allScores;
 	}
 
 	Rail runTurn() {
