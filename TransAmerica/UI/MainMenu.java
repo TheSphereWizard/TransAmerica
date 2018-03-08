4e9727855feba6062e9e1d23e33dee87852f0390
ptewel5086 <ptewel5086@CHHB2ZHHXV1.inst.hcpss.org>
2018-03-08 13:23:51 -0500

    rwe

--- TransAmerica/UI/MainMenu.java
+++ TransAmerica/UI/MainMenu.java
@@ -1,6 +1,16 @@
-import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 //TransAmerica JPanel
 public class MainMenu extends JPanel{
-
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		Image background;
+		
+		g.drawImage(background, 0, 0, null);
+		
+	}
 }
