adeb06f5b724962fc702bfcf7bb24d575ac4c6ee
ptewel5086 <ptewel5086@CHHB2ZHHXV1.inst.hcpss.org>
2018-03-09 14:08:25 -0500

    fregt

--- TransAmerica/UI/ComputerStrategyScreen.java
+++ TransAmerica/UI/ComputerStrategyScreen.java
@@ -1,5 +1,4 @@
 import java.awt.GridLayout;
-
 import javax.swing.*;
 public class ComputerStrategyScreen extends JPanel{
 	private JButton exit = new JButton("Exit");
@@ -7,8 +6,12 @@ public class ComputerStrategyScreen extends JPanel{
 	public ComputerStrategyScreen(int gamesPlayed, int[] gamesWon, int[] gamesLost, int[] rank, double[] winPercentage) {
 		setName("Computer Strategy Analyzer");
 		setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
+		
 		add(exit);
 		add(gameDisplay);
 		setLayout(new GridLayout(3,2,10,0));
+	}
+	private void buttonsLabels(){
+		exit.addActionListener();
 	}
 }
