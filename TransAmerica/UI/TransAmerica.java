afecdb6350f3bd90bc28928c5785d91249dc0b7f
ptewel5086 <ptewel5086@CHHB2ZHHXV1.inst.hcpss.org>
2018-03-09 14:01:48 -0500

    asd

--- TransAmerica/UI/TransAmerica.java
+++ TransAmerica/UI/TransAmerica.java
@@ -4,12 +4,9 @@ public class TransAmerica extends JFrame{
 	public TransAmerica() {
 		setTitle("TransAmerica");
 		setContentPane(new MainMenu());
-<<<<<<< HEAD
 		setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-=======
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
->>>>>>> branch 'master' of https://github.com/TheSphereWizard/TransAmerica
 		setVisible(true);
 	}
 	public void changeContentPane(JPanel newPanel){
