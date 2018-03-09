02fa03de2aab69b42803c0e6f8de7e369ea9d95c
ptewel5086 <ptewel5086@CHHB2ZHHXV1.inst.hcpss.org>
2018-03-09 13:48:35 -0500

    aSWD

--- TransAmerica/UI/TransAmerica.java
+++ TransAmerica/UI/TransAmerica.java
@@ -3,11 +3,14 @@ import javax.swing.JPanel;
 public class TransAmerica extends JFrame{
 	public TransAmerica() {
 		setTitle("TransAmerica");
-		setContentPane(new MainMenu());
+		setContentPane(new ComputerStrategyScreen(0, null, null, null, null));
+		setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 	}
 	public void changeContentPane(JPanel newPanel){
 		setContentPane(newPanel);
+		setTitle(newPanel.getName());
 	}
 	public static void main(String[] args) {
 		new TransAmerica();
