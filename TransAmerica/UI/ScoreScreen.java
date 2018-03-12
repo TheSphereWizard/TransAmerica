ec50a3043d12872b66905398696011e4dcdd5eb9
ptewel5086 <ptewel5086@CHHB2ZQKXV1.inst.hcpss.org>
2018-03-12 08:14:24 -0400

    qaewtewt

--- TransAmerica/UI/ScoreScreen.java
+++ TransAmerica/UI/ScoreScreen.java
@@ -1,46 +1,22 @@
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-@SuppressWarnings("serial")
-
+import javax.swing.*;
 public class ScoreScreen extends JPanel implements ActionListener {
-
 	private JPanel contentPane = new JPanel();
 	private PlayerScorePanel[] playerScorePanels = new PlayerScorePanel[6];
 	private JButton continueButton = new JButton("Continue");
 	private Game currentGame;
-	
 	private boolean gameOver = false;
 	
-	
-	
 	ScoreScreen(ArrayList<Player> players, Game game) {
-		
 		continueButton.addActionListener(this);
 		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
-		
 		currentGame = game;
-		
-		
 		for(Player player: players){
 			
-			
 		}
-		
-		
-		
 	}
-	
-	
-	
-	
-	
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource().equals(continueButton)) {
 			MainGameScreen a = new MainGameScreen(currentGame);
@@ -51,12 +27,5 @@ public class ScoreScreen extends JPanel implements ActionListener {
 	
 	private class PlayerScorePanel extends JPanel {
 
-		private static final long serialVersionUID = 1L;
-	
-		
-		
-		
-		
-		
 	}
 }
