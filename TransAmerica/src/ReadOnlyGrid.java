098a36f8f6530f08ee792bfb318e00f298e0f08d
Sphere Wizard <Sphere Wizard@ComputerofSpheres.fios-router.home>
2018-03-12 18:19:15 -0400

    Added more methods for the players to have access to

--- TransAmerica/src/ReadOnlyGrid.java
+++ TransAmerica/src/ReadOnlyGrid.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class ReadOnlyGrid {
 	private Grid grid;
@@ -6,12 +7,24 @@ public class ReadOnlyGrid {
 	}
 	boolean checkRail(Rail r, Player p){
 		return grid.checkRail(r, p);
+//		return false;
 	}
-	
+	ArrayList<Rail> immediateneighbors(Position p){
+		try {
+			return grid.immediateneighbors(p);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	int distbetweenpoints(Position p1, Position p2){
 		return grid.distbetweenpoints(p1, p2);
 	}
-	
+	ArrayList<Rail> checkRail2(Player p){
+		try{
+			return grid.checkRail2(p);
+		}catch(Exception E){return null;}
+	}
 	boolean RailExists(Position p1, Position p2) {
 		return grid.RailExists(p1, p2);
 	}
