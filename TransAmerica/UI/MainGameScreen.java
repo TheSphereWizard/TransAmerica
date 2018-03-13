4ea8ba0aab0c6793eda8ffe65e8fdb1d726258de
kxu9969 <kxu9969@CHHB2ZPFXV1.inst.hcpss.org>
2018-03-13 13:41:30 -0400

    maingamescreen

--- TransAmerica/UI/MainGameScreen.java
+++ TransAmerica/UI/MainGameScreen.java
@@ -1,5 +1,7 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
@@ -25,6 +27,16 @@ public class MainGameScreen extends JPanel{
 	
 	
 	public MainGameScreen(){
+	}
+	
+	public void generate(ArrayList<Color> playerColors, ArrayList<String> playerNames, ArrayList<String> playerType){
+		ArrayList<Player> players = new ArrayList<Player>();
+		for(int i = 0; i<playerType.size();i++){
+			if(playerType.get(i).equals("Human")){
+				players.add(new HumanPlayer(playerColors.get(i),cities,0,playerNames.get(i),map));
+			}else
+		}
+		
 	}
 	
 	public void paintComponent(Graphics g){
