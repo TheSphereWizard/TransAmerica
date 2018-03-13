b684ee33da6bbaf5db59cd98452492eca73f1cf5
jbosso8565 <jbosso8565@CHHB2ZKDXV1.inst.hcpss.org>
2018-03-13 13:28:58 -0400

    ads

--- TransAmerica/UI/MainGameScreen.java
+++ TransAmerica/UI/MainGameScreen.java
@@ -1,40 +1,59 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-/*
- * MainGameScreen extends JPanel{//Screen used for Human players and slow mode strategy
-	Map map;
-BufferedImage Eagle, Train;
-	Game currentGame;
-	MainGameScreen(Game game){
+public class MainGameScreen extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+
+	//Map map;
+	BufferedImage train , eagle;
+
+	//Game currentGame;
+	private JPanel[] panes = new JPanel[4];
+	private JLabel[] playerLabels = new JLabel[6];
+	private JLabel[] cityLabels = new JLabel[5];
+	private Color[] playerColors = new Color[] {Color.red, Color.yellow, Color.green, Color.blue, new Color(139,69,19),Color.white};
+	private Color[] cityColors = new Color[] {Color.red, Color.blue, Color.yellow, Color.green, Color.orange};
+	private JFrame frame;
+
+	private JLabel transAmericaLabel;
+	
+	MainGameScreen() {
+		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		currentGame = game;
-}
-paintComponent(Graphics g){//calls on getShowScoreScreen in Game, if true changes into the Score Screen, else continues to show MainGameScreen
+		transAmericaLabel = new JLabel("TransAmerica");
+		for(int i = 0; i < panes.length; i++) {
+			this.add(panes[i]);
+		}
+		
+		for(int i = 0; i < playerLabels.length; i++) {
+			playerLabels[i].setText("Player " + i+1);
+			playerLabels[i].setBackground(playerColors[i]);
+			panes[2].setLayout(new GridLayout(1,6));
+			panes[2].add(playerLabels[i]);
+		}
+		
+		for(int i = 0; i < cityLabels.length; i++) {
+			cityLabels[i].setText("City " + i+1);
+			cityLabels[i].setBackground(cityColors[i]);
+			panes[3].setLayout(new GridLayout(1,5));
+			panes[3].add(cityLabels[i]);
+		}
+		
+		panes[0].add(transAmericaLabel);
+		panes[1].add(new MapOfUSA(currentGame.getMap()));
+	}
+	
+	
+	public void paintComponents(Graphics g) {
+		
 	}
 }
 
- */
-public class MainGameScreen extends JPanel{
-	MapofUSA map;
-	BufferedImage train, eagle;
-	Game currentGame;
-	
-	private JPanel[] panes = new JPanel[4];
-	
-	
-	public MainGameScreen(Game game){
-		currentGame = game;
-	}
-	
-	public void paintComponent(Graphics g){
-		
-	}
-	
-	public static void main(String[] args) {
-		
-	}
-	
-	
-}
