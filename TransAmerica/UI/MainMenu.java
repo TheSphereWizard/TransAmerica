aeeb32f2ee5c65325d23db8303b2e1d9ffa582a0
kxu9969 <kxu9969@CHHB2ZPFXV1.inst.hcpss.org>
2018-03-14 14:04:21 -0400

    checked mainmenu

--- TransAmerica/UI/MainMenu.java
+++ TransAmerica/UI/MainMenu.java
@@ -45,7 +45,7 @@ public class MainMenu extends JPanel implements ActionListener{
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		if(e.getActionCommand().equals("Start")) {
+		if(e.getActionCommand().equals("Play")) {
 			int readyPlayers = 0;
 			for(PlayerPanel p:playerPanels){
 				if(p.isPlayer()){
@@ -53,6 +53,7 @@ public class MainMenu extends JPanel implements ActionListener{
 				}
 			}
 			if(readyPlayers>=2){//starts the game, either human or all ai
+				System.out.println("Step 1");
 				ArrayList<PlayerPanel> validPanels = new ArrayList<PlayerPanel>();
 				for(PlayerPanel p:playerPanels){
 					if(p.isPlayer()){
@@ -81,9 +82,18 @@ public class MainMenu extends JPanel implements ActionListener{
 							playerType.add(p.getStrategy());
 						}
 					}
-					MainGameScreen screen = new MainGameScreen();
-					add(screen);
-					screen.generate(playerColors, playerNames, playerType);
+					//MainGameScreen screen = new MainGameScreen();
+					//add(screen);
+					for(int i = 0;i<playerColors.size();i++){
+						System.out.println(playerColors.get(i));
+					}
+					for(int i = 0;i<playerColors.size();i++){
+						System.out.println(playerNames.get(i));
+					}
+					for(int i = 0;i<playerColors.size();i++){
+						System.out.println(playerType.get(i));
+					}
+					//screen.generate(playerColors, playerNames, playerType);
 					System.out.println("Reached!");
 				}else{
 					//progress to ai game
