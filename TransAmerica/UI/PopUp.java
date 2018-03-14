aa29df2d39d3650fc2e7503d03e7f41e606fc908
tkhan0770 <tkhan0770@CHHB2ZMJXV1.inst.hcpss.org>
2018-03-14 13:31:41 -0400

    Popup class

--- TransAmerica/UI/PopUp.java
+++ TransAmerica/UI/PopUp.java
@@ -1,15 +1,58 @@
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class PopUp {
+import javax.swing.*;
+
+public class PopUp extends JPanel{
 	private JPanel popUps;
 	private JPanel aiPopup;
+	private JFrame frame;
+	private JLabel question;
+	private JTextField gameNumber;
+	private JButton fast, slow;
 	
 	public PopUp(){
 		this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
+		question = new JLabel("How many games are to be played?");
+		this.add(question);
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
+		gameNumber = new JTextField();
+		gameNumber.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+			
+		});
+		this.add(gameNumber);
+		
+		fast = new JButton("Fast Mode");
+		fast.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+			
+		});
+		this.add(fast);
+		
+		slow = new JButton("Slow Mode");
+		slow.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+			
+		});
+		this.add(slow);
+		frame.add(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
