e4c5588c5433043e625956b3e325d7af99c7dcba
tkhan007 <tkhan007@Tausif.fios-router.home>
2018-03-13 21:05:33 -0400

    popup

--- TransAmerica/UI/PopUp.java
+++ TransAmerica/UI/PopUp.java
@@ -2,11 +2,11 @@ import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Popup extends JFrame{
+public class PopUp {
 	private JPanel popUps;
 	private JPanel aiPopup;
 	
-	public Popup(){
+	public PopUp(){
 		this.setLayout(new BoxLayout(this,BoxLayout.PAGE_AXIS));
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
