f4a1366c878669aeafb21db63b224a5bae7a64fb
jcole8806 <jcole8806@CHHB2ZQDXV1.inst.hcpss.org>
2018-03-16 13:54:51 -0400

    Fixed format issue

--- TransAmerica/UI/MainMenu.java
+++ TransAmerica/UI/MainMenu.java
@@ -48,7 +48,7 @@ public class MainMenu extends JPanel implements ActionListener{
 			buttons[i].addActionListener(this);
 			buttons[i].setActionCommand(buttons[i].getText());
 			buttons[i].setSize(100,50);
-			buttons[i].setLocation((int) (800+200*(i-.5))-50,750-25);
+			buttons[i].setLocation((int) (800+200*(i-.5))-50,760);
 			this.add(buttons[i]);
 		}
 		try{
