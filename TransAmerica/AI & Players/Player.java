5ab0158a5447b88dcc703430bf4c4fb78dc72e1f
TheSphereWizard <brian.horse@gmail.com>
2018-03-17 18:30:28 -0400

    Moved Record to own class

--- TransAmerica/AI & Players/Player.java
+++ TransAmerica/AI & Players/Player.java
@@ -37,46 +37,5 @@ public abstract class Player {//give players playerrecords
 	public PlayerRecord getPlayerRecord() {
 		return record;
 	}
-	public class PlayerRecord {
-		//Should store player's color, cities, score, and other information as needed.
-		//The players color
-		private Color color;
-		//The cities the player needs/have to connect
-		private ArrayList<City> cities;
-		private ArrayList<City> citiesReached;
-		//# of cities player has connected
-		private int score;
-		private String name;
-		
-		public PlayerRecord(Color color, ArrayList<City> cities, String name) {
-			this.color = color;
-			this.cities = cities;
-			this.score = 12;
-			this.name = name;
-		}
-		String playerName(){
-			return name;
-		}
-		public Color getColor() {
-			return color;
-		}
-		public ArrayList<City> getCities() {
-			return cities;
-		}
-		public void setCities(ArrayList<City> cities){
-			this.cities = cities;
-		}
-		public ArrayList<City> getCitiesReached(){
-			return citiesReached;
-		}
-		public void setCitiesReached(ArrayList<City> citiesReached){
-			this.citiesReached = citiesReached;
-		}
-		public int getScore() {
-			return score;
-		}	
-		public void setScore(int newScore) {
-			score = newScore;
-		}
-	}
+	
 }
---
+++ TransAmerica/AI & Players/PlayerRecord.java
@@ -0,0 +1,45 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class PlayerRecord {
+		//Should store player's color, cities, score, and other information as needed.
+		//The players color
+		private Color color;
+		//The cities the player needs/have to connect
+		ArrayList<City> cities;
+		static ArrayList<City> citiesReached=new ArrayList<City>();
+		//# of cities player has connected
+		private int score;
+		private String name;
+		
+		public PlayerRecord(Color color, ArrayList<City> cities, String name) {
+			this.color = color;
+			this.cities = cities;
+			this.score = 12;
+			this.name = name;
+		}
+		String playerName(){
+			return name;
+		}
+		public Color getColor() {
+			return color;
+		}
+		public ArrayList<City> getCities() {
+			return cities;
+		}
+		public void setCities(ArrayList<City> cities){
+			this.cities = cities;
+		}
+		public ArrayList<City> getCitiesReached(){
+			return citiesReached;
+		}
+		public void setCitiesReached(ArrayList<City> citiesReached){
+			this.citiesReached = citiesReached;
+		}
+		public int getScore() {
+			return score;
+		}	
+		public void setScore(int newScore) {
+			score = newScore;
+		}
+	}
