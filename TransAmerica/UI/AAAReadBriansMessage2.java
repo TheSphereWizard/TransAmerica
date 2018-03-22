33fc08ebe7a12a48195076be890cd6b2bfe56af0
TheSphereWizard <brian.horse@gmail.com>
2018-03-22 15:58:19 -0400

    THIS IS BRIAN'S MESSAGE BECAUSE HE WONT BE THERE TOMMOROW

---
+++ TransAmerica/UI/AAAReadBriansMessage2.java
@@ -0,0 +1,4 @@
+
+public class AAAReadBriansMessage2 {
+
+}
---
+++ TransAmerica/src/AAAMessageFromBrianForTheWeekend.java
@@ -0,0 +1,35 @@
+
+public class AAAMessageFromBrianForTheWeekend {
+	/*
+	 * So hello everyone, I am not here today, 3/22, I am instead on a Chinese Field Trip to New York,
+	 * Good luck with the Demo today, you should be fine as the game as a whole works
+	 * But for over the weekend you guys need to do a ton of work
+	 * - STILL THE UI FOR COMPUTER GAMES AND SCORESCREEN DOESN'T WORK
+	 * - DO LITERALLY NOTHING ELSE TILL THAT IS FIXED
+	 * - SERIOUSLY WHY HAS THIS TAKEN WEEKS!
+	 * 
+	 * If once the Score Screen is done, you have difficulty initiallizing it from Main Game Screen/ itself thats fine, so if to test
+	 * you need to just generate players/game to pass into the ScoreScreen thats fine
+	 * 
+	 * 
+	 * Please Dont forget two things: TIES, and the difference between Round end and Game end
+	 * For Human should go:
+	 * MainMenu
+	 * MainGameScreen
+	 * ScoreScreen:Round
+	 * MainGameScreen
+	 * ScoreScreen:Round
+	 * ...
+	 * MainGameScreen
+	 * ScoreScreen:Round
+	 * ScoreScreen:Game
+	 * MainMenu
+	 * 
+	 * Other things:
+	 * - We need a third strategy, not essential but a better version can be done using Dijkstra's pathfinding algoritum
+	 * 		-if no one knows how to do this I might attempt it over the weekend
+	 * 
+	 * Basically everything needs to be nearly done by Monday as it is due on Tues or Wed, we can't have half of everything
+	 *  (aka the computer portion) still not functioning
+	 */
+}
